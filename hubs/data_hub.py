9a5607ea52e4fae131cfa089ff952694fba1f7da
philehteo <m.theophile.arnould@gmail.com>
2023-07-18 09:57:50 -0500

    COMMIT_DATA

---
+++ hubs/data_hub.py
@@ -0,0 +1,61 @@
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.calibration import LabelEncoder
+
+import sklearn.preprocessing
+import sklearn.model_selection
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split as tts
+
+class Data:
+    """
+    Attributes:
+
+
+    Methods:
+
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def data_process(self, file):
+        ##defining the absolute path for data folder
+        data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','data'))
+
+        ##Find the comlplete path for data file route
+        excel_path = os.path.join(data_dir,file)
+
+        ##load the raw excel file
+        data_raw = pd.read_excel(excel_path , sheet_name=0 )
+
+        ##convert data to array
+        data_arr = np.array(data_raw)
+
+        ##let split the data into feature(inputs) and labels(outputs)
+        data_features = data_arr[:,0:-1]
+        data_labels = data_arr[:,-1]
+
+        #lets label encode any text in the data
+
+        #lets create a boolean array with the size of the columns of the array
+        str_cols = np.empty(data_arr.shape[1],dtype=bool)
+
+        #lets read columns data type
+        for i in range(data_arr.shape[1]):
+            str_cols[i] = np.issubdtype(type(data_arr[0,i]), np.str_)
+
+        for i in range(data_arr.shape[1]):
+                if(str_cols[i]):
+                    le = LabelEncoder()
+                    data_arr[:,i] = le.fit_transform(data_arr[:,i]) +1
+        #lets normalize the data
+        scaler = StandardScaler()#Create an object of this library in particular
+        
+        data_features_normalize = scaler.fit_transform(data_features)
+        data_labels_normalize = scaler.fit_transform(data_labels.reshape(-1,1))
+
+        print(data_features_normalize)
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#main file
+from hubs.data_hub import Data
+
+data = Data()
+data.data_process("ETHEREUM_PRICE.xlsx")
