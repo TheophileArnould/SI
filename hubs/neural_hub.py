002572c16351cb181de6f0eb0c4786fd4311aaa8
Alexic12 <alejandropuerta@outlook.com>
2023-08-10 09:30:35 -0500

    COMMIT_PERCEPTRON_FULL

--- hubs/neural_hub.py
+++ hubs/neural_hub.py
@@ -1,20 +1,20 @@
 
 from hubs.data_hub import Data
-from models.perceptron import Perceptron
+from hubs.models.perceptron import Perceptron
 
 
 class Neural:
     def __init__(self):
         pass
 
-    def run_model(self, model, file_name):
+    def run_model(self, model, file_name, iter, alfa):
         data = Data()
         train_features, test_features, train_labels, test_labels = data.data_process(file_name)
         if model == 'perceptron':
             print('Running Perceptron Model')
             ##Code for the perceptron model
             P = Perceptron()
-            P.run(train_features, test_features, train_labels, test_labels)
+            P.run(train_features, test_features, train_labels, test_labels, iter, alfa)
 
 
         elif model == 'ffm':
